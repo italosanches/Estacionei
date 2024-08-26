c1b02a052b14d277fcbf2bb13c00da300b9aff95
italosanches <italo_carvalho157@hotmail.com>
2024-08-26 17:18:35 -0300

    Adicao de mapping

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Estacionei.DTOs;
+using Estacionei.Models;
+
+namespace Estacionei.Mapping
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile() 
+		{
+			CreateMap<ClienteCreateDto, Cliente>();
+			CreateMap<Cliente, ClienteGetDto>();
+
+			CreateMap<VeiculoDto, Veiculo>();
+		}
+	}
+}
