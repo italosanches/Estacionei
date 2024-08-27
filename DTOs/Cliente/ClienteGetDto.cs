1be0aee7b6ecbaf02c907356036d594c173f8ca1
italosanches <italo_carvalho157@hotmail.com>
2024-08-27 17:24:42 -0300

    Ajuste de pasta

---
+++ DTOs/Cliente/ClienteGetDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Estacionei.DTOs.Cliente
+{
+    public class ClienteGetDto
+    {
+        public int ClienteId { get; set; }
+
+        public string ClienteNome { get; set; }
+
+
+        public string ClienteTelefone { get; set; }
+    }
+}
---
+++ DTOs/Cliente/ClienteUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Estacionei.DTOs.Cliente
+{
+	public class ClienteUpdateDto
+	{
+		[Required]
+		public int ClienteId { get; set; }
+		public string ClienteNome { get; set; }
+		public string ClienteTelefone { get; set; }
+	}
+}
