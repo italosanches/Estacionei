99c85552332d07479eff00980228291d9429079c
italosanches <italo_carvalho157@hotmail.com>
2024-08-28 23:11:49 -0300

    Melhoria de response

--- Response/ResponseBase.cs
+++ Response/ResponseBase.cs
@@ -1,13 +1,18 @@
-﻿namespace Estacionei.Response
+﻿using System.Net;
+
+namespace Estacionei.Response
 {
     public class ResponseBase<T>
     {
         public bool Success { get; set; }
         public T? Data { get; set; }
+        public HttpStatusCode StatusCode { get; set; } 
         public string? Message { get; set; }
 
-        public static ResponseBase<T> SuccessResult(T data,string message) => new ResponseBase<T> { Success = true, Data = data , Message = message};
-        public static ResponseBase<T> FailureResult(string message) => new ResponseBase<T> { Success = false, Message = message };
+        public static ResponseBase<T> SuccessResult(T data, string message,HttpStatusCode statusCode = HttpStatusCode.OK) => new ResponseBase<T> { Success = true, Data = data, Message = message, StatusCode = statusCode};
+        //public static ResponseBase<T> NotFoundResult(string message) => new ResponseBase<T> { Success = false, StatusCode = HttpStatusCode.NotFound, Message = message };
+
+        public static ResponseBase<T> FailureResult(string message,HttpStatusCode statusCode) => new ResponseBase<T> { Success = false, Message = message, StatusCode = statusCode };
     }
 
 }
