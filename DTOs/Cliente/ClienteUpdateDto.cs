c05d8bf508c64ebc902860480bee8231022c2122
italosanches <italo_carvalho157@hotmail.com>
2024-08-29 15:41:48 -0300

    Melhoria de dto, adicionado data anotations

--- DTOs/Cliente/ClienteUpdateDto.cs
+++ DTOs/Cliente/ClienteUpdateDto.cs
@@ -6,7 +6,13 @@ namespace Estacionei.DTOs.Cliente
 	{
 		[Required]
 		public int ClienteId { get; set; }
+
+		[Required(ErrorMessage = "Nome é obrigatorio")]
+		[StringLength(50, ErrorMessage = "O nome deve ter no máximo 50 caracteres.")]
 		public string ClienteNome { get; set; }
+
+		[Required(ErrorMessage = "O telefone é obrigatório.")]
+		[Phone(ErrorMessage = "O telefone deve ser válido.")]
 		public string ClienteTelefone { get; set; }
 	}
 }
