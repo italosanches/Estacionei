5a529f0456f3bd8bf6c6e300e43ef68f69aadb30
italosanches <italo_carvalho157@hotmail.com>
2024-09-05 14:31:26 -0300

    Adicao metodo GetVeiculoByPlaca

--- Repository/VeiculoRepository.cs
+++ Repository/VeiculoRepository.cs
@@ -13,49 +13,10 @@ namespace Estacionei.Repository
         {
             
         }
-        //private readonly AppDbContext _context;
 
-        //public VeiculoRepository(AppDbContext context)
-        //{
-        //	_context = context;
-        //}
-        //public async Task<IEnumerable<Veiculo>> GetAllAsync()
-        //{
-        //	return await _context.Veiculos.AsNoTracking().ToListAsync();
-
-        //}
-        //public async Task<Veiculo?> GetAsync(Expression<Func<Veiculo, bool>> predicate)
-        //      {
-        //	return await _context.Veiculos.AsNoTracking().FirstOrDefaultAsync(predicate);
-
-        //}
-
-        //public async Task<IEnumerable<Veiculo>> FindAsync(Expression<Func<Veiculo, bool>> predicate)
-        //{
-        //	return await _context.Veiculos.AsNoTracking().Where(predicate).ToListAsync();
-        //}
-
-        //public async Task<Veiculo> AddAsync(Veiculo veiculo)
-        //{
-
-        //	await _context.Veiculos.AddAsync(veiculo);
-        //	_context.SaveChanges();
-        //	return veiculo;
-
-        //}
-        //public async Task<Veiculo> UpdateAsync(Veiculo veiculo)
-        //{
-        //	_context.Veiculos.Entry(veiculo).State = EntityState.Modified;
-        //	await _context.SaveChangesAsync();
-        //	return veiculo;
-        //}
-        //public async Task<Veiculo> DeleteAsync(Veiculo veiculo)
-        //{
-        //	_context.Veiculos.Remove(veiculo);
-        //	await _context.SaveChangesAsync();
-        //	return veiculo;
-        //}
-
-
+        public async Task<Veiculo?> GetVeiculoByPlaca(string placa)
+        {
+           return await _context.Veiculos.AsNoTracking().FirstOrDefaultAsync(x => x.VeiculoPlaca == placa.ToUpper());
+        }
     }
 }
