a2304909ef984885bbae4f5008c271d126baa618
italosanches <italo_carvalho157@hotmail.com>
2024-09-06 12:24:23 -0300

    Nova extensão para strings.

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Estacionei.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string RemoveSpecialCharacters(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+
+            Regex regex = new Regex("[^a-zA-Z0-9]");
+
+            return regex.Replace(str, string.Empty);
+        }
+
+    }
+}
