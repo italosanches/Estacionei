7aeda0141e7798b519c4969b2a4dff424ed48ec9
italosanches <italo_carvalho157@hotmail.com>
2024-09-11 17:37:07 -0300

    Reapply "Novos Dtos"
    This reverts commit 510c1c6950d4c4a8b15fc699f6e841ec06ddb103.

---
+++ DTOs/ConfiguracaoValorHora/ConfiguracaoValorHoraCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using Estacionei.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Estacionei.DTOs.ConfiguracaoValorHora
+{
+    public class ConfiguracaoValorHoraCreateDto
+    {
+        [Required (ErrorMessage ="Tipo de veiculo é obrigatorio")]
+        public TipoVeiculo TipoVeiculo { get; set; }
+
+        [Required(ErrorMessage = "Valor por hora é obrigatorio.")]
+
+        public decimal ValorHora { get; set; }
+    }
+}
---
+++ DTOs/ConfiguracaoValorHora/ConfiguracaoValorHoraUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using Estacionei.Enums;
+
+namespace Estacionei.DTOs.ConfiguracaoValorHora
+{
+    public class ConfiguracaoValorHoraUpdateDto 
+    {
+        public int Id { get; set; }
+        public TipoVeiculo TipoVeiculo { get; set; }
+        public decimal ValorHora { get; set; }
+    }
+}
