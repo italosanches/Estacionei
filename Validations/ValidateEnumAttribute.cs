2273f8567509c4f8e606df1dfe9e809d55085d06
italosanches <italo_carvalho157@hotmail.com>
2024-09-14 10:48:40 -0300

    DataAnnotation personalizado para enum

---
+++ Validations/ValidateEnumAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Estacionei.Validations
+{
+    public class ValidateEnumAttribute : ValidationAttribute
+    {
+        private readonly Type _enumType;
+
+        public ValidateEnumAttribute(Type enumType)
+        {
+            _enumType = enumType;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success; // deixando que o Required trate 
+            }
+
+
+            if (!Enum.IsDefined(_enumType,value ))
+            {
+                return new ValidationResult($"O {_enumType.Name} esta incorreto.");
+            }
+
+
+            return ValidationResult.Success;
+        }
+    }
+}
