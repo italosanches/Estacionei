23f363fb69d763ec021cd00a531dc01cd1828f84
italosanches <italo_carvalho157@hotmail.com>
2024-11-01 21:57:08 -0300

    refactor: melhoria de codigo

--- Services/ConfiguracaoValorHoraService.cs
+++ Services/ConfiguracaoValorHoraService.cs
@@ -22,54 +22,54 @@ namespace Estacionei.Services
             _mapper = mapper;
         }
 
-        public async Task<ResponseBase<ConfiguracaoValorHoraGetDto>> CreateConf(ConfiguracaoValorHoraCreateDto confValorHoraCreateDto)
+        public async Task<ResponseBase<ConfiguracaoValorHoraResponseDto>> CreateConf(ConfiguracaoValorHoraRequestDto confValorHoraCreateDto)
         {
             var verificarTipoVeiculo = await _unitOfWork.ConfiguracaoValorHoraRepository.GetAsync(x => x.TipoVeiculo == confValorHoraCreateDto.TipoVeiculo);
             if (verificarTipoVeiculo != null)
             {
-                return ResponseBase<ConfiguracaoValorHoraGetDto>.FailureResult("Configuração para esse tipo de veiculo ja existe no banco de dados.", HttpStatusCode.BadRequest);
+                return ResponseBase<ConfiguracaoValorHoraResponseDto>.FailureResult("Configuração para esse tipo de veiculo ja existe no banco de dados.", HttpStatusCode.BadRequest);
             }
             var configuracaoValorHora = _mapper.Map<ConfiguracaoValorHora>(confValorHoraCreateDto);
             await _unitOfWork.ConfiguracaoValorHoraRepository.AddAsync(configuracaoValorHora);
             await _unitOfWork.Commit();
             await _unitOfWork.Dispose();
-            return ResponseBase<ConfiguracaoValorHoraGetDto>.SuccessResult(_mapper.Map<ConfiguracaoValorHoraGetDto>(configuracaoValorHora), "Configuração criada");
+            return ResponseBase<ConfiguracaoValorHoraResponseDto>.SuccessResult(_mapper.Map<ConfiguracaoValorHoraResponseDto>(configuracaoValorHora), "Configuração criada");
 
         }
 
-        public async Task<ResponseBase<IEnumerable<ConfiguracaoValorHoraGetDto>>> GetAllConf()
+        public async Task<ResponseBase<IEnumerable<ConfiguracaoValorHoraResponseDto>>> GetAllConf()
         {
             var confs = await _unitOfWork.ConfiguracaoValorHoraRepository.GetAllAsync();
-            var confsDto = _mapper.Map<IEnumerable<ConfiguracaoValorHoraGetDto>>(confs);
+            var confsDto = _mapper.Map<IEnumerable<ConfiguracaoValorHoraResponseDto>>(confs);
             if(!confsDto.Any())
             {
-                return ResponseBase<IEnumerable<ConfiguracaoValorHoraGetDto>>.FailureResult("Não há registros no banco.", HttpStatusCode.NotFound);
+                return ResponseBase<IEnumerable<ConfiguracaoValorHoraResponseDto>>.FailureResult("Não há registros no banco.", HttpStatusCode.NotFound);
 
             }
-            return ResponseBase<IEnumerable<ConfiguracaoValorHoraGetDto>>.SuccessResult(confsDto ?? new List<ConfiguracaoValorHoraGetDto>(), "Configurações encontradas.");
+            return ResponseBase<IEnumerable<ConfiguracaoValorHoraResponseDto>>.SuccessResult(confsDto ?? new List<ConfiguracaoValorHoraResponseDto>(), "Configurações encontradas.");
         }
 
-        public async Task<ResponseBase<ConfiguracaoValorHoraGetDto>> GetById(int id)
+        public async Task<ResponseBase<ConfiguracaoValorHoraResponseDto>> GetById(int id)
         {
             var result = await _unitOfWork.ConfiguracaoValorHoraRepository.GetAsync(conf => conf.Id == id);
             if (result is null)
             {
-                return ResponseBase<ConfiguracaoValorHoraGetDto>.FailureResult("Não foi encontrado.", HttpStatusCode.NotFound);
+                return ResponseBase<ConfiguracaoValorHoraResponseDto>.FailureResult("Não foi encontrado.", HttpStatusCode.NotFound);
             }
-            return ResponseBase<ConfiguracaoValorHoraGetDto>.SuccessResult(_mapper.Map<ConfiguracaoValorHoraGetDto>(result), "Configuração encontrada.");
+            return ResponseBase<ConfiguracaoValorHoraResponseDto>.SuccessResult(_mapper.Map<ConfiguracaoValorHoraResponseDto>(result), "Configuração encontrada.");
         }
 
-        public async Task<ResponseBase<ConfiguracaoValorHoraGetDto>> GetByTipoVeiculo(TipoVeiculo tipoVeiculo)
+        public async Task<ResponseBase<ConfiguracaoValorHoraResponseDto>> GetByTipoVeiculo(TipoVeiculo tipoVeiculo)
         {
             var result = await _unitOfWork.ConfiguracaoValorHoraRepository.GetAsync(conf => conf.TipoVeiculo == tipoVeiculo);
             if (result is null)
             {
-                return ResponseBase<ConfiguracaoValorHoraGetDto>.FailureResult("Não há configuração para esse tipo de veiculo.", HttpStatusCode.NotFound);
+                return ResponseBase<ConfiguracaoValorHoraResponseDto>.FailureResult("Não há configuração para esse tipo de veiculo.", HttpStatusCode.NotFound);
             }
-            return ResponseBase<ConfiguracaoValorHoraGetDto>.SuccessResult(_mapper.Map<ConfiguracaoValorHoraGetDto>(result), "Configuração encontrada.");
+            return ResponseBase<ConfiguracaoValorHoraResponseDto>.SuccessResult(_mapper.Map<ConfiguracaoValorHoraResponseDto>(result), "Configuração encontrada.");
         }
 
-        public async Task<ResponseBase<bool>> UpdateConf(ConfiguracaoValorHoraUpdateDto confValorHoraUpdateDto)
+        public async Task<ResponseBase<bool>> UpdateConf(ConfiguracaoValorHoraRequestDto confValorHoraUpdateDto)
         {
             var result = await _unitOfWork.ConfiguracaoValorHoraRepository.GetAsync(conf => conf.TipoVeiculo == confValorHoraUpdateDto.TipoVeiculo);
 
@@ -79,7 +79,7 @@ namespace Estacionei.Services
             }
             if(result.Id !=  confValorHoraUpdateDto.Id)
             {
-                return ResponseBase<bool>.FailureResult("Ids divergentes da configuração existente para esse tipo de veiculo.", HttpStatusCode.BadRequest);
+                return ResponseBase<bool>.FailureResult("Ids divergentes da configuração existente para esse tipo de veiculo.", HttpStatusCode.Conflict);
 
             }
             _unitOfWork.ConfiguracaoValorHoraRepository.UpdateAsync(_mapper.Map<ConfiguracaoValorHora>(confValorHoraUpdateDto));
