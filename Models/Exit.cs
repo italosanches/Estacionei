146356410101aa7bca239757c877cada43295331
italosanches <italo_carvalho157@hotmail.com>
2024-11-21 21:52:30 -0300

    Refactored: Refatorado models

---
+++ Models/Exit.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Estacionei.Models
+{
+    public class Exit
+    {
+        public int ExitId { get; set; }
+        public int EntryId { get; set; }
+
+        public DateTime ExitDate { get; set; } = DateTime.MinValue;
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal ChargedAmount { get; set; }
+
+        public virtual Entry Entry { get; set; }
+    }
+
+}
