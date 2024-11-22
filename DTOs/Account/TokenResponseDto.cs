a9216eb54d70f588a18940466cdf54d0a481c39c
italosanches <italo_carvalho157@hotmail.com>
2024-11-21 21:52:03 -0300

    Feat: Nova DTO

---
+++ DTOs/Account/TokenResponseDto.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Estacionei.DTOs.Authentication
+{
+    public class TokenResponseDto : TokenDto
+    {
+        public DateTime? Expiration { get; set; }
+    }
+}
